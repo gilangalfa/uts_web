a00c4dddb44ba974188d96cd3b06bb84715b910d
gilangalfa <122207319+gilangalfa@users.noreply.github.com>
2024-04-18 15:41:29 +0800

    Add files via upload

--- index.php
+++ index.php
@@ -1,50 +1,22 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title>SAMPLE KODING FROM KIOS MALASNGODING</title>
-	<link rel="stylesheet" type="text/css" href="assets/css/bootstrap.css">
-	<link rel="stylesheet" type="text/css" href="assets/js/jquery-ui/jquery-ui.css">
-	<script type="text/javascript" src="assets/js/jquery.js"></script>
-	<script type="text/javascript" src="assets/js/bootstrap.js"></script>
-	<script type="text/javascript" src="assets/js/jquery-ui/jquery-ui.js"></script>
-	<?php include 'admin/config.php'; ?>
-	<style type="text/css">
-	.kotak{	
-		margin-top: 150px;
-	}
+<?php 
+include 'header.php';
+?>
 
-	.kotak .input-group{
-		margin-bottom: 20px;
-	}
-	</style>
-</head>
-<body>	
-	<div class="container">
-		<?php 
-		if(isset($_GET['pesan'])){
-			if($_GET['pesan'] == "gagal"){
-				echo "<div style='margin-bottom:-55px' class='alert alert-danger' role='alert'><span class='glyphicon glyphicon-warning-sign'></span>  Login Gagal !! Username dan Password Salah !!</div>";
-			}
-		}
-		?>
-		<div class="panel panel-default">
-			<form action="login_act.php" method="post">
-				<div class="col-md-4 col-md-offset-4 kotak">
-					<h3>Silahkan Login ..</h3>
-					<div class="input-group">
-						<span class="input-group-addon"><span class="glyphicon glyphicon-user"></span></span>
-						<input type="text" class="form-control" placeholder="Username" name="uname">
-					</div>
-					<div class="input-group">
-						<span class="input-group-addon"><span class="glyphicon glyphicon-lock"></span></span>
-						<input type="password" class="form-control" placeholder="Password" name="pass">
-					</div>
-					<div class="input-group">			
-						<input type="submit" class="btn btn-primary" value="Login">
-					</div>
-				</div>
-			</form>
-		</div>
-	</div>
-</body>
-</html>
+<?php
+$a = mysql_query("select * from barang_laku");
+?>
+
+<div class="col-md-10">
+	<h3>Selamat datang</h3>	
+    <h3>Aplikasi Penjualan Sederhana</h3>
+    <h3>Jangan Lupa Subscribe</h3>
+</div>
+<!-- kalender -->
+<div class="pull-right">
+	<div id="kalender"></div>
+</div>
+
+<?php 
+include 'footer.php';
+
+?>
